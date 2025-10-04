d336558c78fbdc5b0bfa66fb1f590074f95af3ab
Anurag Shrestha <anuragshrestha16@gmail.com>
2025-10-04 12:16:44 -0600

    rearrange the screens based on the design

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import './styles/ui.css'
 
 import Header from './components/Header.jsx'
+import TitleBar from './components/TitleBar.jsx'
 import MobileTabs from './components/MobileTabs.jsx'
 
 import Home from './screens/Home.jsx'
 import AirQualityIndex from './screens/AirQualityIndex.jsx'
+import AirQualityAlerts from './screens/AirQualityAlerts.jsx'
 import CityRankings from './screens/CityRankings.jsx'
 import WildfireUpdates from './screens/WildfireUpdates.jsx'
 import DataSources from './screens/DataSources.jsx'
@@ -15,12 +17,21 @@ function App() {
     <div className="app-root">
       <MobileTabs />
       <div className="container">
+        <TitleBar />
         <Header />
-        <Home />
-        <AirQualityIndex />
-        <CityRankings />
-        <WildfireUpdates />
-        <DataSources />
+        <div className="dashboard-grid">
+          <div className="stack">
+            <AirQualityAlerts />
+            <CityRankings />
+          </div>
+          <div>
+            <Home />
+          </div>
+          <div className="stack">
+            <WildfireUpdates />
+            <DataSources />
+          </div>
+        </div>
       </div>
     </div>
   )
