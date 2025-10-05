d6962e1b0078cf811d8a9d2a37dcad8e85314c12
Anurag Shrestha <anuragshrestha16@gmail.com>
2025-10-04 18:33:17 -0600

    updated ui for mobile screen size

--- src/components/MobileTabs.jsx
+++ src/components/MobileTabs.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
 
 export default function MobileTabs() {
   return (
     <nav className="mobile-tabs" aria-label="Mobile navigation">
-      <a href="#aqi">Air Quality Index</a>
+      <a href="#home">Satellite View</a>
+      <a href="#alerts">Air Quality Alerts</a>
       <a href="#rankings">City Rankings</a>
       <a href="#wildfires">Wildfire Updates</a>
       <a href="#sources">Data Sources</a>
